870d05b04f40ef485cd17e3f70a1483d66c57310
dangnhap2589 <65329898+dangnhap2589@users.noreply.github.com>
2020-07-21 02:06:15 +0700

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,4 +1,4 @@
-var socket = io.connect('https://chatsocketio0002.herokuapp.com');
+var socket = io.connect('https://chatsocketio0002.herokuapp.com:8000');
 socket.on('connect', function (data) {
     socket.emit('join', 'Hello server from client');
 });
